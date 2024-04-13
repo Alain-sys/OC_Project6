802917fd93b6987db96297847ae1658759b21879
Alain Remi <Alain-sys@protonmail.com>
2024-04-13 11:50:39 +0200

    style: slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -26,14 +26,37 @@ function Slider({ accommodationImageList }) {
   const accommodationImage = accommodationImageList[index];
 
   return (
-    <div>
-      <button type="button" onClick={handlePrevClick}>
-        <img src={sliderLeftArrow} alt="icon left arrow" />
+    <div className="slider">
+      <button
+        type="button"
+        className="slider__btn slider__btn--prev"
+        onClick={handlePrevClick}
+      >
+        <img
+          className="slider__btn__icon"
+          src={sliderLeftArrow}
+          alt="icon left arrow"
+        />
       </button>
-      <img src={accommodationImage} alt="accommodation" />
-      <button type="button" onClick={handleNextClick}>
-        <img src={sliderRightArrow} alt="icon right arrow" />
+      <img
+        className="slider__image"
+        src={accommodationImage}
+        alt="accommodation"
+      />
+      <button
+        type="button"
+        className="slider__btn slider__btn--next"
+        onClick={handleNextClick}
+      >
+        <img
+          className="slider__btn__icon"
+          src={sliderRightArrow}
+          alt="icon right arrow"
+        />
       </button>
+      <p className="slider__image--count">
+        {index + 1}/{accommodationImageList.length}
+      </p>
     </div>
   );
 }
